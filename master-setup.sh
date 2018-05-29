8146b37184a9712570f627d1241a517596002d28
Steven Roach <stevenwroach@hotmail.com>
2018-05-29 14:27:16 -0700

    Update master-setup.sh

--- master-setup.sh
+++ master-setup.sh
@@ -1,48 +1,83 @@
 #!/bin/bash
 
-set -x
+set -xeuo pipefail
 
 if [[ $(id -u) -ne 0 ]] ; then
     echo "Must be run as root"
     exit 1
 fi
 
+if [ $# != 2 ]; then
+    echo "Usage: $0 <MasterHostname> <TemplateBaseUrl>"
+    exit 1
+fi
+
+# Set user args
+MASTER_HOSTNAME=$1
+TEMPLATE_BASE_URL="$2"
 
 # Shares
 SHARE_HOME=/share/home
-NFS_DATA=/share/data
+SHARE_DATA=/share/data
 
-# User
+
+# Hpc User
 HPC_USER=hpcuser
 HPC_UID=7007
-HPC_GROUP=hpc
-HPC_GID=7007
-
-MASTER_NAME=`hostname`
+HPC_GROUP=users
 
 
+# Returns 0 if this node is the master node.
+#
+is_master()
+{
+    hostname | grep "$MASTER_HOSTNAME"
+    return $?
+}
+
+# Add the SLES 12 SDK repository which includes all the
+# packages for compilers and headers.
+#
+add_sdk_repo()
+{
+    repoFile="/etc/zypp/repos.d/SMT-http_smt-azure_susecloud_net:SLE-SDK12-Pool.repo"
+	
+    if [ -e "$repoFile" ]; then
+        echo "SLES 12 SDK Repository already installed"
+        return 0
+    fi
+	
+	wget $TEMPLATE_BASE_URL/sles12sdk.repo
+	
+	cp sles12sdk.repo "$repoFile"
+
+    # init new repo
+    zypper -n search nfs > /dev/null 2>&1
+}
 
 # Installs all required packages.
 #
 install_pkgs()
 {
-	yum -y install nfs-utils nfs-utils-lib
+    pkgs="libbz2-1 libz1 openssl libopenssl-devel gcc gcc-c++ nfs-client rpcbind"
+
+    if is_master; then
+        pkgs="$pkgs nfs-kernel-server"
+    fi
+
+    zypper -n install $pkgs
 }
 
-
-
-# Partitions all data disks attached to the VM 
+# Partitions all data disks attached to the VM and creates
+# a RAID-0 volume with them.
 #
 setup_data_disks()
 {
     mountPoint="$1"
-    filesystem="$2"
-    devices="$3"
-    raidDevice="$4"
-    createdPartitions=""
+	createdPartitions=""
 
     # Loop through and partition disks until not found
-    for disk in $devices; do
+    for disk in sdc sdd sde sdf sdg sdh sdi sdj sdk sdl sdm sdn sdo sdp sdq sdr; do
         fdisk -l /dev/$disk || break
         fdisk /dev/$disk << EOF
 n
@@ -50,120 +85,100 @@ p
 1
 
 
-p
+t
+fd
 w
 EOF
         createdPartitions="$createdPartitions /dev/${disk}1"
-    done
-    
-    sleep 10
+	done
 
-# Create RAID-0 volume
+    # Create RAID-0 volume
     if [ -n "$createdPartitions" ]; then
         devices=`echo $createdPartitions | wc -w`
         mdadm --create /dev/md10 --level 0 --raid-devices $devices $createdPartitions
-        mkfs -t ext4 /dev/md10
-        echo "/dev/md10 $mountPoint ext4 defaults,nofail 0 2" >> /etc/fstab
-        mount /dev/md10
+	    mkfs -t ext4 /dev/md10
+	    echo "/dev/md10 $mountPoint ext4 defaults,nofail 0 2" >> /etc/fstab
+	    mount /dev/md10
+    fi
+}
+
+# Creates and exports two shares on the master nodes:
+#
+# /share/home (for HPC user)
+# /share/data
+#
+# These shares are mounted on all worker nodes.
+#
+setup_shares()
+{
+    mkdir -p $SHARE_HOME
+    mkdir -p $SHARE_DATA
+
+    if is_master; then
+	    setup_data_disks $SHARE_DATA
+        echo "$SHARE_HOME    *(rw,async)" >> /etc/exports
+        echo "$SHARE_DATA    *(rw,async)" >> /etc/exports
+        service nfsserver status && service nfsserver reload || service nfsserver start
+    else
+        echo "master:$SHARE_HOME $SHARE_HOME    nfs4    rw,auto,_netdev 0 0" >> /etc/fstab
+        echo "master:$SHARE_DATA $SHARE_DATA    nfs4    rw,auto,_netdev 0 0" >> /etc/fstab
+        mount -a
+        mount | grep "^master:$SHARE_HOME"
+        mount | grep "^master:$SHARE_DATA"
+    fi
+}
+
+
+
+
+
+
+# Adds a common HPC user to the node and configures public key SSh auth.
+# The HPC user has a shared home directory (NFS share on master) and access
+# to the data share.
+#
+setup_hpc_user()
+{
+    if is_master; then
+        useradd -c "HPC User" -g $HPC_GROUP -d $SHARE_HOME/$HPC_USER -s /bin/bash -m -u $HPC_UID $HPC_USER
+
+        # Configure public key auth for the HPC user
+        sudo -u $HPC_USER ssh-keygen -t rsa -f $SHARE_HOME/$HPC_USER/.ssh/id_rsa -q -P ""
+        cat $SHARE_HOME/$HPC_USER/.ssh/id_rsa.pub > $SHARE_HOME/$HPC_USER/.ssh/authorized_keys
+
+        echo "Host *" > $SHARE_HOME/$HPC_USER/.ssh/config
+        echo "    StrictHostKeyChecking no" >> $SHARE_HOME/$HPC_USER/.ssh/config
+        echo "    UserKnownHostsFile /dev/null" >> $SHARE_HOME/$HPC_USER/.ssh/config
+		echo "    PasswordAuthentication no" >> $SHARE_HOME/$HPC_USER/.ssh/config
+
+        chown $HPC_USER:$HPC_GROUP $SHARE_HOME/$HPC_USER/.ssh/authorized_keys
+        chown $HPC_USER:$HPC_GROUP $SHARE_HOME/$HPC_USER/.ssh/config
+        chown $HPC_USER:$HPC_GROUP $SHARE_DATA
+    else
+        useradd -c "HPC User" -g $HPC_GROUP -d $SHARE_HOME/$HPC_USER -s /bin/bash -u $HPC_UID $HPC_USER
     fi
 
-#	mkfs -t $filesystem $createdPartitions
-#	echo "$createdPartitions $mountPoint $filesystem defaults,nofail 0 2" >> /etc/fstab
-	
-#	mount $createdPartitions
-}
-
-setup_disks()
-{      
-    # Dump the current disk config for debugging
-    fdisk -l
-    
-    # Dump the scsi config
-    lsscsi
-    
-    # Get the root/OS disk so we know which device it uses and can ignore it later
-    rootDevice=`mount | grep "on / type" | awk '{print $1}' | sed 's/[0-9]//g'`
-    
-    # Get the TMP disk so we know which device and can ignore it later
-    tmpDevice=`mount | grep "on /mnt/resource type" | awk '{print $1}' | sed 's/[0-9]//g'`
-
-    # Get the data disk sizes from fdisk, we ignore the disks above
-    dataDiskSize=`fdisk -l | grep '^Disk /dev/' | grep -v $rootDevice | grep -v $tmpDevice | awk '{print $3}' | sort -n -r | tail -1`
-
-	# Compute number of disks
-	nbDisks=`fdisk -l | grep '^Disk /dev/' | grep -v $rootDevice | grep -v $tmpDevice | wc -l`
-	echo "nbDisks=$nbDisks"
-	
-	dataDevices="`fdisk -l | grep '^Disk /dev/' | grep $dataDiskSize | awk '{print $2}' | awk -F: '{print $1}' | sort | head -$nbDisks | tr '\n' ' ' | sed 's|/dev/||g'`"
-    
-    mkdir -p $SHARE_HOME
-	mkdir -p $NFS_DATA
-	setup_data_disks $NFS_DATA "xfs" "$dataDevices" "nfsdata"
-
-    chown $HPC_USER:$HPC_GROUP $NFS_DATA
-	
-	echo "$NFS_DATA    *(rw,async)" >> /etc/exports
-
-    echo "$SHARE_HOME  *(rw,async)" >> /etc/exports
-    
-    systemctl enable rpcbind || echo "Already enabled"
-    systemctl enable nfs-server || echo "Already enabled"
-    systemctl start rpcbind || echo "Already enabled"
-    systemctl start nfs-server || echo "Already enabled"
-	exportfs
-	exportfs -a
-	exportfs 
-}
-
-
-setup_user()
-{
-    # disable selinux
-    sed -i 's/enforcing/disabled/g' /etc/selinux/config
-    setenforce permissive
-    
-    groupadd -g $HPC_GID $HPC_GROUP
-    
     # Don't require password for HPC user sudo
     echo "$HPC_USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-    
-    # Disable tty requirement for sudo
-    sed -i 's/^Defaults[ ]*requiretty/# Defaults requiretty/g' /etc/sudoers
-   
-	useradd -c "HPC User" -g $HPC_GROUP -m -d $SHARE_HOME/$HPC_USER -s /bin/bash -u $HPC_UID $HPC_USER
-
-	mkdir -p $SHARE_HOME/$HPC_USER/.ssh
-
-	# Configure public key auth for the HPC user
-	ssh-keygen -t rsa -f $SHARE_HOME/$HPC_USER/.ssh/id_rsa -q -P ""
-	cat $SHARE_HOME/$HPC_USER/.ssh/id_rsa.pub >> $SHARE_HOME/$HPC_USER/.ssh/authorized_keys
-
-	echo "Host *" > $SHARE_HOME/$HPC_USER/.ssh/config
-	echo "    StrictHostKeyChecking no" >> $SHARE_HOME/$HPC_USER/.ssh/config
-	echo "    UserKnownHostsFile /dev/null" >> $SHARE_HOME/$HPC_USER/.ssh/config
-	echo "    PasswordAuthentication no" >> $SHARE_HOME/$HPC_USER/.ssh/config
-
-	# Fix .ssh folder ownership
-	chown -R $HPC_USER:$HPC_GROUP $SHARE_HOME/$HPC_USER
-
-	# Fix permissions
-	chmod 700 $SHARE_HOME/$HPC_USER/.ssh
-	chmod 644 $SHARE_HOME/$HPC_USER/.ssh/config
-	chmod 644 $SHARE_HOME/$HPC_USER/.ssh/authorized_keys
-	chmod 600 $SHARE_HOME/$HPC_USER/.ssh/id_rsa
-	chmod 644 $SHARE_HOME/$HPC_USER/.ssh/id_rsa.pub
-	
-	# chown -R $HPC_USER:$HPC_GROUP $NFS_DATA
 }
 
+# Sets all common environment variables and system parameters.
+#
+setup_env()
+{
+    # Set unlimited mem lock
+    echo "$HPC_USER hard memlock unlimited" >> /etc/security/limits.conf
+	echo "$HPC_USER soft memlock unlimited" >> /etc/security/limits.conf
 
+	# Intel MPI config for IB
+    echo "# IB Config for MPI" > /etc/profile.d/hpc.sh
+	echo "export I_MPI_FABRICS=shm:dapl" >> /etc/profile.d/hpc.sh
+	echo "export I_MPI_DAPL_PROVIDER=ofa-v2-ib0" >> /etc/profile.d/hpc.sh
+	echo "export I_MPI_DYNAMIC_CONNECTION=0" >> /etc/profile.d/hpc.sh
+}
 
+add_sdk_repo
 install_pkgs
-setup_disks
-setup_user
-
-
-exit 0
-
-
-
+setup_shares
+setup_hpc_user
+setup_env
